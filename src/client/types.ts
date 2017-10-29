10607fb482cc47f1daf71610cd162c9c87e59334
Kyle Brown <picokyle@gmail.com>
2017-10-29 13:17:37 -0400

    Remove broken link from documentation

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,7 +1,7 @@
 /**
  * The kinds of insights that can be requested from Watson Natural Language
  * Understanding. The user sets these using the [[QueryOptionsSelector]]
- * component, and the current settings are managed by the [[currentQuery]]
+ * component, and the current settings are managed by the `currentQuery`
  * reducer.
  */
 export type QueryOptions =
